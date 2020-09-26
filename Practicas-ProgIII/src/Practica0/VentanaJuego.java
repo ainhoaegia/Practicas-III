6b814ed09035edd87c8f0bfba2181f6f730eaff5
ainhoaegia <ainhoa.egia@icloud.com>
2020-09-26 17:26:59 +0200

    Paso 1

--- Practicas-ProgIII/src/Practica0/VentanaJuego.java
+++ Practicas-ProgIII/src/Practica0/VentanaJuego.java
@@ -1,9 +1,66 @@
 package Practica0;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
 public class VentanaJuego extends JFrame{
+	
 	public static void main(String[] args) {
+		VentanaJuego ventana = new VentanaJuego( "PRÁCTICA 0" );
+		ventana.setVisible(true);
+	}
+	
+	private JPanel panelCentro;
+	private JPanel panelSur;
+	private JButton botonAcelera;
+	private JButton botonFrena;
+	private JButton botonGiraIzq;
+	private JButton botonGiraDrc;
+	
+	public VentanaJuego (String titulo) {
 		
+		super(titulo);
+
+		// VENTANA
+
+		this.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		this.setSize( 900, 300 );
+		this.setLocation( 260, 120 );
+		this.setLocationRelativeTo( null );
+		getContentPane().revalidate();
+
+		// CONTENEDORES Y COMPONENTES
+		
+		panelCentro = new JPanel();
+		panelSur = new JPanel();
+		
+		// PANEL CENTRO
+		
+		this.setLayout( null ); // LayOut nulo
+		this.add( panelCentro, BorderLayout.CENTER );
+		
+		// PANEL SUR --> BOTONERA
+		
+		botonAcelera = new JButton( "Acelera" );
+		botonFrena = new JButton( "Frena" );
+		botonGiraIzq = new JButton( "Gira Izquierda" );
+		botonGiraDrc = new JButton( "Gira Derecha" );
+
+		botonAcelera.setFocusable( true );
+		botonFrena.setFocusable( true );
+		botonGiraIzq.setFocusable( true );
+		botonGiraDrc.setFocusable( true );
+
+		panelCentro.add( botonAcelera );
+		panelCentro.add( botonFrena );
+		panelCentro.add( botonGiraIzq );
+		panelCentro.add( botonGiraDrc );
+		
+		this.setLayout( null );
+		this.add( panelSur, BorderLayout.SOUTH );
 	}
 }
